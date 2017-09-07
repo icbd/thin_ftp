f71bc3760786be2d2001df72ab8435e4c26c256b
Baodong <wwwicbd@gmail.com>
2017-09-07 14:57:02 +0800

    MultiProcess

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
 require 'logger'
 require_relative 'server/serial'
+require_relative 'server/multi_process'
 
 $logger = Logger.new("log/server-#{Time.now.to_s[0..10]}.log")
 
-server = FTP::Serial.new(3000)
+server = FTP::MultiProcess.new(3000)
 server.run
---
+++ server/multi_process.rb
@@ -0,0 +1,56 @@
+require 'socket'
+require_relative '../ftp/protocol_handler'
+
+module FTP
+  class MultiProcess
+
+    attr_reader :client
+
+    def initialize(server_port = 21)
+      @control_socket = TCPServer.new(server_port)
+
+      Signal.trap(:INT) { exit }
+    end
+
+    def gets
+      client.gets(CRLF)
+    end
+
+    def respond(msg)
+      client.write(msg + CRLF)
+    end
+
+    def run
+      puts "服务启动"
+
+      # 循环接受连接
+      loop do
+        @client = @control_socket.accept
+
+        pid = fork do
+          respond "200 你好"
+          handler = ProtocolHandler.new(self)
+
+          # 循环处理命令
+          loop do
+            request = gets
+
+            if request
+              respond handler.handle request
+            else
+              @client.close
+              break
+            end
+
+          end
+
+        end
+
+        # 分离子进程, 防止变僵尸进程
+        Process.detach pid
+      end
+
+    end
+
+  end
+end
